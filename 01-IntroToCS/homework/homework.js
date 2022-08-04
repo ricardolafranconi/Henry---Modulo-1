1657c24e4d419c1ed6a59c24296ef8a616791c2a
unknown <lafranconiricardo@gmail.com>
2022-08-04 19:12:38 -0300

    homework done

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,12 +1,20 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+  
+  let decimal = 0
+  for (let i=0; i < num.length; i++){
+    decimal += num[i]*2**(num.length -1 -i) }
+    return decimal
 }
-
+  
 function DecimalABinario(num) {
-  // tu codigo aca
+  let binario = ''
+  while(num !== 0) {
+    binario = num%2 + binario  
+   num = Math.floor(num/2)
+  }
+  return binario
 
 }
 

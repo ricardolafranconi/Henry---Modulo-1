6bb2f1c9d4d178e4a4f8671688c4bd8ef9fec5c6
unknown <lafranconiricardo@gmail.com>
2022-08-11 19:57:58 -0300

    Homework done

--- 03-JavaScriptAvanzado-II/homework/homework.js
+++ 03-JavaScriptAvanzado-II/homework/homework.js
@@ -28,12 +28,34 @@ function counter() {
 }
 
 function cacheFunction(cb) {
+  let cache = {}  
+  return function(arg){
+      if(!cache.hasOwnProperty(arg)){
+      cache[arg] = cb(arg);
+      return cache[arg]
+    }
+      
+    }
+  }
+
+
   /*
   Ejercicio 2
 
-  Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché para el callback que recibe por parámetro (cb); es decir, que "recuerde" el resultado de cada operación que hace, de manera que, al realizar una operación por segunda vez, se pueda obtener el resultado de esa "memoria" sin tener que efectuar otra vez cálculos que ya se hicieron anteriormente.
+  Tu tarea aquí es lograr, mediante un closure, 
+  que cacheFunction actúe como una memoria caché para el callback 
+  que recibe por parámetro (cb); es decir, que "recuerde" 
+  el resultado de cada operación que hace, de manera que, 
+  al realizar una operación por segunda vez, se pueda obtener
+   el resultado de esa "memoria" sin tener que efectuar otra vez cálculos que ya se hicieron anteriormente.
 
-  cacheFunction debe retornar una función. Esta función debe aceptar un argumento (arg) e invocar a cb con ese argumento; hecho eso, debe guardar el argumento junto con el resultado de la invocación (tip: usá un objeto donde cada propiedad sea el argumento, y su valor el resultado de la correspondiente invocación a cb) de manera que, la próxima vez que reciba el mismo argumento, no sea necesario volver a invocar a cb, porque el resultado estará guardado en la "memoria caché".
+  cacheFunction debe retornar una función. 
+  Esta función debe aceptar un argumento (arg) e invocar a cb con ese argumento; 
+  hecho eso, debe guardar el argumento junto con el resultado de la invocación 
+  (tip: usá un objeto donde cada propiedad sea el argumento, y 
+    su valor el resultado de la correspondiente invocación a cb) de manera que, 
+    la próxima vez que reciba el mismo argumento, no sea necesario volver a invocar a cb, 
+    porque el resultado estará guardado en la "memoria caché".
 
 
   Ejemplo:
@@ -44,10 +66,11 @@ function cacheFunction(cb) {
   const squareCache = cacheFunction(square)
 
   squareCache(5)    // invocará a square(5), almacenará el resultado y lo retornará
-  squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) 
+  squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) 
+                        y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) 
 
   */
-}
+
 
 // Bind
 
@@ -73,8 +96,8 @@ function getNombre() {
   Usando el método bind() guardar, en las dos variables declaradas a continuación, dos funciones que actúen como getNombre pero retornen el nombre del instructor y del alumno, respectivamente.
 */
 
-let getNombreInstructor;
-let getNombreAlumno;
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno)
 
 /*
   Ejercicio 4
@@ -86,9 +109,9 @@ function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena) {
   return delimitadorIzquierda + cadena + delimitadorDerecha;
 }
 
-let textoAsteriscos;
-let textoGuiones;
-let textoUnderscore;
+let textoAsteriscos = crearCadena.bind(null, '*', '*');
+let textoGuiones = crearCadena.bind(null, '-', '-');
+let textoUnderscore = crearCadena.bind(null, '_', '_');
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
@@ -102,3 +125,4 @@ module.exports = {
   textoGuiones,
   textoUnderscore,
 };
+
